dab5ae0a78e257e7f00dabdea92931c5ac80e47e
Sashko Vysokovskiy <thettboy11@gmail.com>
2024-05-22 12:02:04 +0300

    chkNested should not throw an error if no nested markers are found

--- controllersError.test.js
+++ controllersError.test.js
@@ -21,3 +21,10 @@ describe('Error Controllers', () => {
             const text = 'This is **bold _italic_** text';
             expect(() => chkNested(text, /\*\*(.+?)\*\*/g, '**')).toThrow('Nested markers are not allowed');
         });
+
+        it('should not throw an error if no nested markers are found', () => {
+            const text = 'This is **bold** text';
+            expect(() => chkNested(text, /\*\*(.+?)\*\*/g, '**')).not.toThrow();
+        });
+    });
+});
