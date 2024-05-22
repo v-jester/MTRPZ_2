b2612aa7df329f6a395014af395452832663182a
Sashko Vysokovskiy <thettboy11@gmail.com>
2024-05-22 12:09:44 +0300

    Regular expressions to match opening and closing markers for bold, italic, and monospace

--- reader.js
+++ reader.js
@@ -2,3 +2,10 @@ const bReg = /(?<=[ ,.:;\n\t]|^)\*\*(?=\S)(.+?)(?<=\S)\*\*(?=[ ,.:;\n\t]|$)/g;
 const iReg = /(?<=[ ,.:;\n\t]|^)_(?=\S)(.+?)(?<=\S)_(?=[ ,.:;\n\t]|$)/g;
 const mReg = /(?<=[ ,.:;\n\t]|^)`(?=\S)(.+?)(?<=\S)`(?=[ ,.:;\n\t]|$)/g;
 const regs = [bReg, iReg, mReg];
+
+const lbReg = /(?<=[ ,.:;\n\t]|^)\*\*(?=\S)/g;
+const rbReg = /(?<=\S)\*\*(?=[ ,.:;\n\t]|$)/g;
+const liReg = /(?<=[ ,.:;\n\t]|^)_(?=\S)/g;
+const riReg = /(?<=\S)_(?=[ ,.:;\n\t]|$)/g;
+const lmReg = /(?<=[ ,.:;\n\t]|^)`(?=\S)/g;
+const rmReg = /(?=\S)`(?=[ ,.:;\n\t]|$)/g;
