efbca7da70c7902bd89f070ec2d01201d782c4ee
Sashko Vysokovskiy <thettboy11@gmail.com>
2024-05-22 11:43:00 +0300

    Implement nested marker validation

--- controllersError.js
+++ controllersError.js
@@ -15,3 +15,36 @@ const chkClosed = (text, lReg, rReg, reg) => {
         throw new Error('There is no closing marker');
     }
 };
+
+const chkNested = (text, reg, mark) => {
+    
+    const parts = text.match(reg);
+    if (parts) {
+        for (const part of parts) {
+            const slice = part.slice(mark.length, -mark.length);
+            if (
+                slice.length > 2 &&
+                (markers.includes(slice[0] + slice[1]) ||
+                markers.includes(
+                    slice[slice.length - 1] + slice[slice.length - 2]
+                ))
+            ) {
+                throw new Error('Nested markers are not allowed');
+            }
+            if (
+                slice.length > 1 &&
+                (markers.includes(slice[0]) ||
+                markers.includes(slice[slice.length - 1]))
+            ) {
+                throw new Error('Nested markers are not allowed');
+            }
+            if (
+                slice.match(/\*\*/g)?.length > 1 ||
+                slice.match(/_/g)?.length > 1 ||
+                slice.match(/`/g)?.length > 1
+            ) {
+                throw new Error('Nested markers are not allowed');
+            }
+        }
+    }
+};
