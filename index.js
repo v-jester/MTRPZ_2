3d48881df10f07431d1785bde93dbcfdf5bd3dff
Sashko Vysokovskiy <thettboy11@gmail.com>
2024-05-22 13:48:22 +0300

    failed test yml

--- index.js
+++ index.js
@@ -11,11 +11,5 @@ if (!file) {
     throw new Error('No file path provided'); 
 }
 (async () => {
-    const mdText = await fs.readFile(file, 'utf-8'); 
-    if (outIdx !== -1 && process.argv[outIdx + 1]) {
-        const outFile = path.resolve(process.argv[outIdx + 1]); 
-        await fs.writeFile(outFile, format === 'html' ? mdToHtml(mdText) : mdToAnsi(mdText)); 
-    } else {
-        console.log(format === 'html' ? mdToHtml(mdText) : mdToAnsi(mdText)); 
-    }
+
 })();
