d27844606104183f7d0d035b5922496a83adb746
Sashko Vysokovskiy <thettboy11@gmail.com>
2024-05-22 12:48:47 +0300

    Default format is 'ansi' if not specified

--- index.js
+++ index.js
@@ -5,3 +5,4 @@ import { mdToHtml, mdToAnsi } from './reader.js';
 const file = process.argv[2];
 const outIdx = process.argv.indexOf('--out');
 const formatIdx = process.argv.indexOf('--format');
+const format = formatIdx !== -1 ? process.argv[formatIdx + 1] : 'ansi';
