5aca16b03705effdc425ba2b39763f67a6b4a5d0
Sashko Vysokovskiy <thettboy11@gmail.com>
2024-05-22 12:41:31 +0300

    Test for converting markdown to HTML

--- reader.test.js
+++ reader.test.js
@@ -3,3 +3,11 @@ import fs from 'fs/promises';
 import { exec } from 'child_process';
 
 describe('Markdown Parser', () => {
+
+    test('should convert markdown to HTML', () => {
+        const mdText = 'This is **bold**, _italic_, and `monospaced` text.';
+        const result = mdToHtml(mdText);
+        expect(result).toContain('<b>bold</b>');
+        expect(result).toContain('<i>italic</i>');
+        expect(result).toContain('<tt>monospaced</tt>');
+    });
